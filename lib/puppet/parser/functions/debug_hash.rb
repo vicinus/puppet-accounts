ed09eb586ad6427605c51262805e57580778b509
Reinhard Vicinus <r.vicinus@metaways.de>
2015-07-13 10:00:16 +0200

    removed debug_hash function

--- lib/puppet/parser/functions/debug_hash.rb
+++
@@ -1,6 +0,0 @@
-module Puppet::Parser::Functions
-  newfunction(:debug_hash, :type => :rvalue) do |args|
-    raise Puppet::Error, "debug_hash requires 1 argument; got #{args.length}" if args.length != 1
-    return args[0].inspect
-  end
-end
