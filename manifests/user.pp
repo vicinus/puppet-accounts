f34fb2d387978107294538265178bc6e887608f4
Reinhard Vicinus <r.vicinus@metaways.de>
2015-08-18 13:55:07 +0200

    fix for strict vars option

--- manifests/user.pp
+++ manifests/user.pp
@@ -105,6 +105,7 @@ define accounts::user(
       }
       $ssh_keys_require = File[$real_ssh_keys_location]
     } else {
+      $real_ssh_keys_location = undef
       $ssh_keys_require = Accounts::Home_dir[$home]
     }
     create_resources('ssh_authorized_key', make_hash($ssh_keys, "${name}_"), {
